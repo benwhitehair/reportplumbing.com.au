949bdffb88eac5989a8ba9e8acfe91d4eac2492e
Luke Bennett <luke.bennett88@gmail.com>
2018-11-22 20:02:45 +1100

    Change teal colour

--- assets/css/tailwind.js
+++ assets/css/tailwind.js
@@ -90,7 +90,7 @@ let colors = {
   "teal-darkest": "#0d3331",
   "teal-darker": "#20504f",
   "teal-dark": "#06839E",
-  teal: "#4dc0b5",
+  teal: "#adccd4",
   "teal-light": "#64d5ca",
   "teal-lighter": "#a0f0ed",
   "teal-lightest": "#e8fffe",
